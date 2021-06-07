2030a88e3034bf1082f23d751bdc42c9ed6a645d
RiceX <datph.zes@gmail.com>
2021-06-08 02:29:25 +0700

    failed when

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,21 @@ var router = express.Router();
 
 /* GET home page   */
 router.get('/', function(req, res)  {
-  var abc = req.session.key;  
-  if(req.session.key) 
-  res.render('todo.html', {abc : abc })
+  var abc = req.session.key;
+  var xyz = {...abc}
+  xyz = JSON.stringify(abc);
+  console.log(xyz);
+  console.log(typeof(xyz))
+  if(req.session.key)   
+  res.render('todo.html', {abc : xyz})
 });
 router.post('/', function(req, res) {
-    req.session.key.push(req.body);
-    console.log( req.session.key);
+
+
+
+    req.session.key.push(JSON.parse(JSON.stringify(req.body)));
+    // console.log(req.body)
+    // console.log(req.session.key);
     res.json(req.body.myWork) 
 })
 
