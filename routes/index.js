1fd43a310f84cabb9271aa99a2d16396b95886a4
Gao <datph.zes@gmail.com>
2021-06-08 12:21:53 +0700

    chưa thay được giá trị trong session

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,34 @@ var router = express.Router();
 var abc = [];
 /* GET home page   */
 router.get('/', function(req, res)  {
-  abc = req.session.key;
-  var xyz = {abc}
-  xyz = JSON.stringify(abc);
+  abc = req.session.key; // Gán mảng abc = value session.key   
+  var xyz = {abc}  //  Session bị bọc bởi mảng nên cần loại bỏ để không bị lỗi lúc truyền sang client;
+  xyz = JSON.stringify(abc); // String Json - JSON
     // console.log(xyz);
     // console.log(typeof(xyz))
   if(req.session.key)   
   res.render('todo.html', {abc : xyz})
 });
 router.post('/', function(req, res) {
-    req.session.key.push(JSON.parse(JSON.stringify(req.body)));
+    req.session.key.push(JSON.parse(JSON.stringify(req.body))); // delete [Object:  undefined ]
     // console.log(req.body)
     // console.log(req.session.key);
-    res.json(req.body.myWork) 
+    res.json(req.body.myWork)
+})
+router.post('/checkbox', function(req, res){
+  const chk = req.body.checkbox;
+  console.log(chk);
+    if (req.session.key.checkedbox == false)
+    req.session.key.checkedbox = true;
+    else 
+    req.session.key.checkedbox = false;
+    // Change html ...
 })
 router.post('/post', function(req, res) {
     const del = req.body.delId;
     console.log(del);
     abc.splice(del,1);
     console.log(abc);
-    req.session.key = abc ;    
     // xyz.splice(del, 1);
     // console.log(xyz);
 })
