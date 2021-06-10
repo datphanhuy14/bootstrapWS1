9d26b473cdc160bab09cad12450bdf3154eef747
Gao <datph.zes@gmail.com>
2021-06-10 23:33:22 +0700

    DONE

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const nunjucks = require('nunjucks');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const redis = require('redis')
-var client = redis.createClient()
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const nunjucks = require("nunjucks");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const redis = require("redis");
+var client = redis.createClient();
 // const session = require('express-session')
 const app = express();
 
@@ -16,41 +16,39 @@ const app = express();
 var idList = [];
 var listValue = [];
 // view engine setup
-app.set('view engine', 'html');
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
 });
 
-client.on("error", function(error) {
+client.on("error", function (error) {
   console.error(error);
-});        
-
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-
-  if (err){
-    console.log('false')
-    }else{
-    console.log(err)
+app.use(function (req, res, next) {
+  if (err) {
+    console.log("false");
+  } else {
+    console.log(err);
   }
   next(createError(404));
 });
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error' , {err : err.message});
+  res.render("error", { err: err.message });
 });
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,28 @@ const redis = require("redis").createClient();
 var redisScan = require("redisscan");
 const async = require("async");
 
-var listValue = [];
-var keyId = [];
-
 router.get("/", function (req, res) {
-   // REdis Get
-    var jobs = [];
-    redis.keys('*', function (err, keys) {
-        if (err) return console.log(err);
-        if(keys){
-            async.map(keys, function(key, cb) {
-               redis.get(key, function (error, value) {
-                    if (error) return cb(error);
-                    var todo = {};
-                    todo['key']=key;
-                    todo['data']=value;
-                    cb(null, todo);
-                }); 
-            }, function (error, results) {
-               if (error) return console.log(error);
-               console.log(typeof JSON.stringify(results));
-               res.render('home.html',{ data :JSON.stringify(results)});  // ! chuyển định giạng object của results sang string để truyển qua biến data về client
-            });
-        }
-    });
-
+  // REdis Get
+  var todoList = [];
+  redis.keys("todoList_*", function (err, keys) {
+    if (err) return console.log(err);
+    async.map(
+      keys,
+      function (key, cb) {
+        redis.get(key, function (error, value) {
+          if (error) return cb(error);
+          // console.log(value);
+          todoList.push(JSON.parse(value));
+          cb();
+          console.log(todoList)
+        });
+      },
+      function (error, results) {
+        if (error) return console.log(error);
+        res.render("home.html", { data: todoList });
+      }
+    );
+  });
 });
 // Post data &
 router.post("/", function (req, res) {
@@ -37,23 +34,48 @@ router.post("/", function (req, res) {
     JSON.stringify({
       id: req.body.id,
       myWork: req.body.myWork,
-      checkbox: req.body.checkedbox,
+      checkbox: req.body.checkbox,
+      check: req.body.check,
     }),
-    function (err, rep) {
-      keyId.push(req.body.id);
-      console.log(keyId);
-    }
-  );    
-  // redis.get(`todoList_${req.body.id}`, function (err, reply) {
-  //   listValue.push(reply);
-  //   console.log(reply);                // TODO:Lúc refresh lại server thì không thể get được listValue. 
-  //   console.log(listValue);
-  // });
-  res.json(req.body.myWork);    
+  );
+  res.json(req.body.myWork); // Tra ve gia tri myWord(content)
 });
 router.post("/checkbox", function (req, res) {
-  // Change  ...
+  // Change key
+  console.log(req.body.checkStatus);
+  console.log(req.body.idCheck);
+  console.log(req.body.myWork);
+  // let del2 = `todoList_${req.body.idCheck}` // Del key
+  // redis.del(del2)                           // Del key
+
+    // Nếu Trạng thái bằng True thì bật CHECKBOX
+  if (req.body.checkbox == 0) {
+      check = 1;
+    redis.set(`todoList_${req.body.idCheck}`, JSON.stringify({   // Ghi đè lên key redis đang có sẵn
+      id: req.body.idCheck,      // id Check = id 
+      myWork: req.body.myWork,      // Content
+      checkbox: 1,              
+      check: "checked",   // String checked 
+    })
+    );
+  }
+    // NẾU Trạng thái = false thì tắt checkbox 
+  else if (req.body.checkbox == 1) {
+    redis.set(`todoList_${req.body.idCheck}`, JSON.stringify({
+      id: req.body.idCheck,
+      myWork: req.body.myWork,
+      checkbox: "0",
+      check: " ",  
+    })
+    );
+  }
+  else console.log("Error");
 });
-router.post("/post", function (req, res) {});
+ //
+router.post("/post", function (req, res) {
+  var delKey = "todoList_" + req.body.delId;
+  console.log(delKey);
+  redis.del(delKey);
+});
 
 module.exports = router;
